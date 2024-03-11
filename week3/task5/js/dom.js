bba9a24b86f6631c2b9fd6b015e0ef9bad48ce91
truongnguyen2911 <anhtruong.works@gmail.com>
2024-03-11 21:56:12 +1000

    week 3 task 5

---
+++ week3/task5/js/dom.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    const fetchPromise = fetch("http://api.imgflip.com/get_memes");
+    fetchPromise.then(respone => {
+        if (respone.ok) {
+            return respone.json();
+        } else {
+            throw new Error(respone.status);
+        }
+    }).then(data => {
+        //console.log("data: ", data.data.memes);
+        //$("#meme-images").html("<p>" + data.data.memes[0].url + "</p>");
+        displayPhotos(data.data.memes, 10);
+    }).catch(error => {
+        console.error(error);
+    });
+});
+
+function displayPhotos(urls, number) {
+    var photoList = document.getElementById("photoList");
+
+    photoList.innerHTML = "";
+
+    urls.slice(0, number).forEach(e => {
+        var photoBox = document.createElement("div");
+        photoBox.classList.add("photo-box");
+
+        // Create an anchor element for the photo link
+        var photoLink = document.createElement("a");
+        photoLink.href = e.url;
+
+        // Create an image element
+        var imageElement = document.createElement("img");
+        imageElement.src = e.url;
+        imageElement.width = 200;
+        imageElement.height = 200;
+
+
+        // Append the image, figcaption, link, box to the anchor element
+        photoLink.appendChild(imageElement);
+        photoBox.appendChild(photoLink);
+        photoList.appendChild(photoBox);
+    });
+}

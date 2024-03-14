00f52d2173070228a41bcc6291f77e7501bb689d
Anh Truong Nguyen <truongnguyen4042@gmail.com>
2024-03-14 23:01:47 +1000

    change image api in week 3 task 3

--- week3/task3/js/dom.js
+++ week3/task3/js/dom.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
-    $.get("http://quotes.rest/qod.json?category=inspire", function(data) {
+    $.get("https://api.nasa.gov/planetary/apod?api_key=UvoMW3iN0toRkSBkvcuy6Dtr53VwZdWKdq4Fowww", function(data) {
         console.log("data: ", data);
-        $("#quote-jq").html("<p>" + data + "</p>");
+        $("#quote-jq").html("<img src= '"+ data.url +"'>");
     }, "json");
 
-    const fetchPromise = fetch("http://quotes.rest/qod.json?category=inspire");
+    const fetchPromise = fetch("https://api.nasa.gov/planetary/apod?api_key=UvoMW3iN0toRkSBkvcuy6Dtr53VwZdWKdq4Fowww");
     fetchPromise.then(respone => {
         if (respone.ok) {
             return respone.json();
@@ -12,8 +12,9 @@ $(document).ready(function () {
             throw new Error(respone.status);
         }
     }).then(data => {
-        $("#quote-jq").html("<p>" + data + "</p>");
+        console.log("url: ", data.hdurl);
+        $("#quote-fetch").html("<img src= '"+ data.hdurl +"'>");
     }).catch(error => {
         console.error(error);
     });
-});
+ });
